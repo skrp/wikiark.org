55ab5893e6568bbe0484adf0a05c878741289a23
Prince of Archives <rich.tijerina@gmail.com>
2017-03-20 04:56:09 -0600

    Update TOILET.pl

--- TOILET.pl
+++ TOILET.pl
@@ -1,2 +1,37 @@
 #!/usr/local/bin/perl
 use strict; use warnings;
+##########################
+# TOILET - dump html form
+get '/' => sub {
+	my $c = shift;
+	return $c->render(template => 'index');
+};
+app->start('daemon', '-l', 'http://*:6664');
+__DATA__
+@@ error.html.ep
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" >
+    <title>Error</title>
+  </head>
+  <body>
+    <%= $message %>
+  </body>
+</html>
+@@ index.html.ep
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" >
+    <title>MKRX WEB DUMP</title>
+  </head>
+  <body>
+    <h1>WEB FILE DUMP</h1>
+    <form method="post" action="<%= url_for('upload') %>" enctype ="multipart/form-data">
+      <div>
+        Select File
+        <input type="file" name="file" >
+        <input type="submit" value="Upload" >
+      </div>
+    </form>
+  </body>
+</html>
