9ef511927ff7af330d822c00de22b48c0264eb56
Prince of Archives <rich.tijerina@gmail.com>
2017-03-20 04:44:16 -0600

    Update ROWDY.pl

--- ROWDY.pl
+++ ROWDY.pl
@@ -1,2 +1,12 @@
 #!/usr/local/bin/perl
 use strict; use warnings;
+#########################
+# ROWDY - raw http server
+get '/:id' => sub {
+  my $c = shift;
+  my $id = $c->stash('id');
+  my $loc = $dir . $id;
+  return $c->render(message => 'PASTE NO EXIST') unless -e $loc;
+  my $paste = read_file($loc);
+  $c->render(text => $paste, format => 'txt');
+};
