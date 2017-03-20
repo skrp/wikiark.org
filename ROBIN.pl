224f841f1b1354e51ca6fa28beed620f1282da05
Prince of Archives <rich.tijerina@gmail.com>
2017-03-20 04:42:47 -0600

    Update ROBIN.pl

--- ROBIN.pl
+++ ROBIN.pl
@@ -1,2 +1,18 @@
 #!/usr/local/bin/perl
 use strict; use warnings;
+use Mojolicious::Lite;
+######################
+# ROBIN - raw http bin
+post '/', sub {
+  my $c = shift;
+  my $paste = $c->param('paste');
+  $c->stash(paste => '');
+  return $c->render(message => 'PASTE FAILED') unless $paste;
+  my $enc_paste = Mojo::ByteStream->new($paste)->encode('utf-8');
+  my $pre_id = sha1_hex($enc_paste);
+  my $id = substr($pre_id, 0, 6);
+  my $loc = $dir . $id;
+  open(my $pfh, '>', $loc);
+  print $pfh $enc_paste;
+  $c->render(text => "wikiark.org/raw/$id");
+};
